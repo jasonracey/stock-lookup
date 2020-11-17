b420c7f3215a685a28ef7e0bcc349fb9ac5738cf
Jason Racey <jasonracey@gmail.com>
2020-11-16 22:52:52 -0800

    Move loading indicator into app jsx

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,6 +1,27 @@
 import React from "react";
 import List from "./List";
 import Form from "./Form";
+import { usePromiseTracker } from "react-promise-tracker";
+import Loader from 'react-loader-spinner';
+
+const LoadingIndicator = () => {
+    const { promiseInProgress } = usePromiseTracker();
+  
+    return(
+      promiseInProgress &&
+      <div
+        style={{
+          width: "100%",
+          height: "100",
+          display: "flex",
+          justifyContent: "left",
+          alignItems: "left"
+        }}
+      >
+        <Loader type="ThreeDots" color="#00A4EF" height="75" width="75" />
+      </div>
+    );
+  }
 
 const App = () => (
     <>
@@ -11,8 +32,8 @@ const App = () => (
         <div>
             <h2>Stock Prices</h2>
             <List />
+            <LoadingIndicator/>
         </div>
-
     </>
 );
 
